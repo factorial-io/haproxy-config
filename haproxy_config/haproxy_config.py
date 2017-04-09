23075ffd5feae4634aba77418a1483cc57136317
Stephan Maximilian Huber <stephan@factorial.io>
2017-04-09 19:51:45 +0200

    Code-style

--- haproxy_config/haproxy_config.py
+++ haproxy_config/haproxy_config.py
@@ -40,8 +40,17 @@ def write_config():
 
     logging.info('found {name} with ip {ip}, using {vhost}:{port} as hostname.'.format(name=name, ip=ip, vhost=vhost, port=port))
 
-    frontends += "    acl host_{name} hdr(host) -i {vhost}\n    use_backend {name}_cluster if host_{name}\n".format(name=name,vhost=vhost)
-    backends += "\n\nbackend {name}_cluster\n    server node1 {ip}:{port}\n".format(name=name,ip=ip, port=port)
+    frontends += """
+    acl host_{name} hdr(host) -i {vhost}
+    use_backend {name}_cluster if host_{name}
+""".format(name=name,vhost=vhost)
+
+    backends += """
+
+backend {name}_cluster
+    mode http
+    server node1 {ip}:{port}
+""".format(name=name,ip=ip, port=port)
 
     if ssl:
       certs = certs + "crt " + ssl + " "
@@ -51,27 +60,32 @@ def write_config():
       logging.info('using SSL with cert {cert}'.format(cert=ssl))
 
     if redirect:
-      frontends += "    acl redirect_host_{name} hdr(host) -i {redirect}\n    redirect code 301 prefix http://{vhost} if redirect_host_{name}\n".format(name=name,vhost=vhost,redirect=redirect)
+      frontends += """    acl redirect_host_{name} hdr(host) -i {redirect}
+    redirect code 301 prefix http://{vhost} if redirect_host_{name}
+""".format(name=name,vhost=vhost,redirect=redirect)
 
   with open('/usr/local/etc/haproxy/haproxy.cfg', 'w+') as out:
-      for line in open('./haproxy-override/haproxy.in.cfg'):
-          if line.strip() == "###FRONTENDS###":
-              out.write(frontends)
-          elif line.strip() == "###BACKENDS###":
-              out.write(backends)
-	  elif line.strip() == "###CERTS###":
-            if certs != '':
-              out.write("    bind *:443 ssl %s\n" % certs)
+    for line in open('./haproxy-override/haproxy.in.cfg'):
+      if line.strip() == "###FRONTENDS###":
+        out.write(frontends)
+      elif line.strip() == "###BACKENDS###":
+        out.write(backends)
+      elif line.strip() == "###CERTS###":
+        if certs != '':
+          out.write("    bind *:443 ssl %s\n    mode http\n" % certs)
 
-          elif line.strip() == "###HTTPS_FRONTENDS###":
-              out.write(https_frontends)
-          else:
-              out.write(line)
+      elif line.strip() == "###HTTPS_FRONTENDS###":
+        out.write(https_frontends)
+      elif line.strip() == '###SSH_PROXY###':
+        out.write(ssh_proxy)
+      else:
+        out.write(line)
+
   logging.info('Restarting haproxy container')
   #os.system("haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /run/haproxy.pid -sf $(cat /run/haproxy.pid)")
   os.system("kill -s HUP $(pidof haproxy-systemd-wrapper)")
   time.sleep(5)
-	
+
   #os.system("service haproxy reload")
 
 class MyEventHandler(FileSystemEventHandler):
